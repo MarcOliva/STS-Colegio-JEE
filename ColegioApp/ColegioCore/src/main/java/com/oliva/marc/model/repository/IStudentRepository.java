ad0a3f04b1955306213da5896629907daccab40f
DIEGO <DIEGO@Beast>
2019-04-28 22:58:08 -0500

    Commit 3 (EducationLevel class deleted)

--- ColegioApp/ColegioCore/src/main/java/com/oliva/marc/model/repository/IStudentRepository.java
+++ ColegioApp/ColegioCore/src/main/java/com/oliva/marc/model/repository/IStudentRepository.java
@@ -3,5 +3,5 @@ package com.oliva.marc.model.repository;
 import com.oliva.marc.model.entity.Student;
 
 public interface IStudentRepository extends IJpaRepository<Student> {
-
+	
 }
--- ColegioApp/ColegioCore/src/main/java/com/oliva/marc/service/IEducationLevelService.java
+++
@@ -1,7 +0,0 @@
-package com.oliva.marc.service;
-
-import com.oliva.marc.model.entity.EducationLevel;
-
-public interface IEducationLevelService extends ICrudService<EducationLevel> {
-
-}
